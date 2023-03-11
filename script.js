88ebf4db472648770b9c508d511c223d85e6a849
Chí Hữu <huu10a10vts@gmail.com>
2023-03-11 21:27:56 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.addEventListener("click", function (){
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+})
+
+
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.addEventListener("click", function (){
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+})
+
